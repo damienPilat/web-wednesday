8c9d1d74520e8a5d14946eaf266469c6ef89fb64
damienPilat <dspilat@hotmail.com>
2025-11-05 21:03:01 +0000

    add base type configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,16 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: [
+        "./apps/landing-page/tsconfig.json",
+        "./apps/london-cycle-stations/tsconfig.json",
+        "./apps/portfolio/tsconfig.json",
+        "./apps/web/tsconfig.json"
+    ],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
   extends: ["@repo/eslint-config/index.js"],
 };
